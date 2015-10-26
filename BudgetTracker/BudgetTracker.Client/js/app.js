7abb198454248c8849f806cb32474b941dc18221
jeffward01 <jeffwardcoding@gmail.com>
2015-10-26 15:29:41 -0700

    added homepage tranition

--- BudgetTracker/BudgetTracker.Client/js/app.js
+++ BudgetTracker/BudgetTracker.Client/js/app.js
@@ -1,12 +1,17 @@
-﻿$(document).ready(function () {
+﻿
+//Intro Page
+$(document).ready(function () {
     $('.mainContainer').hide();
 
     $('#reveal').on('click', function () {
         $('.Intro').fadeOut('slow').then(
             $('.mainContainer').fadeIn('slow')
         );
-  
-       
     })
+});
+
+//Go Home Button
+$(".homeBtn").on('click', function () {
+    $('.mainContainer').fadeOut('slow').then($('.Intro').fadeIn('slow'));
 
 })
